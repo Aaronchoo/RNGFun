c4a0e810b77929b8d712ca20743239cae3a47e67
Iconic <aaronchoo9@gmail.com>
2017-07-31 11:57:26 -0400

    Adding more activities

---
+++ RNGFun/app/src/main/java/com/example/android/rngfun/GiveRandomNumbers.java
@@ -0,0 +1,13 @@
+package com.example.android.rngfun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiveRandomNumbers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_give_random_numbers);
+    }
+}
