a0cfdc7c74e7694c3ca4c342774ef8ef4f21ba9a
Iconic <aaronchoo9@gmail.com>
2017-08-16 14:03:56 -0400

    Different features and better visual change
    Added a reset option  that will remove all previous data, and added one more textview to ensure the details of the choice is not as large as the choice.

--- RNGFun/app/src/main/java/com/example/android/rngfun/NameActivity.java
+++ RNGFun/app/src/main/java/com/example/android/rngfun/NameActivity.java
@@ -24,18 +24,20 @@ public class NameActivity extends AppCompatActivity {
         ;
         final Button chooseOne = (Button) findViewById(R.id.choose_option);
         final EditText nameChoice = (EditText) findViewById(R.id.name_iteam);
+        final Button resetAll = (Button) findViewById(R.id.reset_all);
         ;
         final EditText detailChoice = (EditText) findViewById(R.id.name_details);
         ;
         final TextView showChoice = (TextView) findViewById(R.id.chosen_one);
+        final TextView showDetail = (TextView) findViewById(R.id.chosen_detail);
         //Create an array list that will hold the list of names and details
         final ArrayList<ListDetails> choices = new ArrayList<>();
         //Sets an onclicklistener taht will react when they add a choice
         addChoices.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Check to see if they have actually added info
-                if ((nameChoice.getText().toString().equals("") && detailChoice.getText().toString().equals("")))
+                //Check to see if they have actually added info (The only important one is the name (first input))
+                if ((nameChoice.getText().toString().equals("")))
                     //If not, tell usersto add info
                     Toast.makeText(NameActivity.this, "Add info!", Toast.LENGTH_SHORT).show();
                 else{
@@ -65,10 +67,27 @@ public class NameActivity extends AppCompatActivity {
                     //Cast it to get an integer
                     int randomChoice = (int) random;
                     //Display random choice
-                    showChoice.setText(choices.get(randomChoice).getName() + '\n' + choices.get(randomChoice).getDetails());
+                    showChoice.setText(choices.get(randomChoice).getName());
+                    //Display the detail of it too (Did two different textviews because the details aren't as important
+                    //and show not be as big)
+                    showDetail.setText(choices.get(randomChoice).getDetails());
                 }
                 //If not do nothing!
 
+            }
+        });
+        //If the reset button is clicked
+        resetAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Clear the elements
+                choices.clear();;
+                //Erase the list
+                ListView listChoice = (ListView) findViewById(R.id.list);
+                listChoice.setAdapter(null);
+                //Set the textviews that may have content to show nothing
+                showChoice.setText("");
+                showDetail.setText("");
             }
         });
 
